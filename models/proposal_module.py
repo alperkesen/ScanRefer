0b1425801da9cf800e7097e1cded131011776adc
alperkesen <kesen.alper@gmail.com>
2021-05-26 19:40:54 +0300

    Add proposal modules for refinement

--- models/proposal_module.py
+++ models/proposal_module.py
@@ -11,6 +11,7 @@ import sys
 
 sys.path.append(os.path.join(os.getcwd(), "lib")) # HACK add the lib folder
 import lib.pointnet2.pointnet2_utils
+from utils.pc_utils import rotation_3d_in_axis
 
 class ProposalModule(nn.Module):
     def __init__(self, num_class, num_heading_bin, num_size_cluster, mean_size_arr):
@@ -20,7 +21,7 @@ class ProposalModule(nn.Module):
         self.num_heading_bin = num_heading_bin
         self.num_size_cluster = num_size_cluster
         self.mean_size_arr = mean_size_arr
-            
+
         # Object proposal/detection
         # Objectness scores (2), center residual (3),
         # heading class+residual (num_heading_bin*2), size class+residual(num_size_cluster*4)
@@ -43,7 +44,7 @@ class ProposalModule(nn.Module):
         """
 
         features = data_dict['aggregated_vote_features'].permute(0, 2, 1).contiguous() # (batch_size, 128, num_proposal)
-        # --------- PROPOSAL GENERATION ---------        
+        # --------- PROPOSAL GENERATION ---------
         net = self.proposal(features)
         data_dict = self.decode_scores(net, data_dict, self.num_class, self.num_heading_bin, self.num_size_cluster, self.mean_size_arr)
 
@@ -84,3 +85,161 @@ class ProposalModule(nn.Module):
 
         return data_dict
 
+
+class ClassAgnosticProposalModule(nn.Module):
+    def __init__(self, num_class, num_heading_bin):
+        super().__init__()
+
+        self.num_class = num_class
+        self.num_heading_bin = num_heading_bin
+
+        # Object proposal/detection
+        # Objectness scores (2), center residual (3),
+        # heading class+residual (num_heading_bin*2)
+        self.proposal = nn.Sequential(
+            nn.Conv1d(128,128,1, bias=False),
+            nn.BatchNorm1d(128),
+            nn.ReLU(),
+            nn.Conv1d(128,128,1, bias=False),
+            nn.BatchNorm1d(128),
+            nn.ReLU(),
+            nn.Conv1d(128,2+6+num_heading_bin*2+self.num_class,1)
+        )
+
+    def forward(self, data_dict):
+        """
+        Args:
+            data_dict
+        Returns:
+            scores: (B,num_proposal,2 (sem) + 6 (dist) + num_class + num_dir_bins * 2) 
+        """
+
+        features = data_dict['aggregated_vote_features'].permute(0, 2, 1).contiguous() # (batch_size, 128, num_proposal)
+        # --------- PROPOSAL GENERATION ---------
+        net = self.proposal(features)
+        data_dict = self.decode_scores(net, data_dict, self.num_class, self.num_heading_bin)
+
+        return data_dict
+
+    def decode_scores(self, net, data_dict, num_class, num_heading_bin):
+        """
+        decode the predicted parameters for the bounding boxes
+
+        """
+        net_transposed = net.transpose(2,1).contiguous() # (batch_size, 1024, ..)
+        batch_size = net_transposed.shape[0]
+        num_proposal = net_transposed.shape[1]
+
+        objectness_scores = net_transposed[:,:,0:2]
+        distance = net_transposed[:,:,2:8] # (batch_size, num_proposal, 3)
+
+        heading_scores = net_transposed[:,:,8:8+num_heading_bin]
+        heading_residuals_normalized = net_transposed[:,:,8+num_heading_bin:8+num_heading_bin*2]
+
+        sem_cls_scores = net_transposed[:,:,8+num_heading_bin*2:] # Bxnum_proposalx10
+
+        # store
+        data_dict['objectness_scores'] = objectness_scores
+        data_dict['distance'] = distance
+        data_dict['heading_scores'] = heading_scores # Bxnum_proposalxnum_heading_bin
+        data_dict['heading_residuals_normalized'] = heading_residuals_normalized # Bxnum_proposalxnum_heading_bin (should be -1 to 1)
+        data_dict['heading_residuals'] = heading_residuals_normalized * (np.pi/num_heading_bin) # Bxnum_proposalxnum_heading_bin
+        data_dict['sem_cls_scores'] = sem_cls_scores
+
+        return data_dict
+
+    def decode(self, bbox_out, mode='rpn'):
+        assert mode in ['rpn', 'rcnn']
+        prefix = 'refined_' if mode == 'rcnn' else ''
+
+        distance = bbox_out[prefix+'distance']  # (B, N, 6)
+        batch_size, num_proposal, _ = distance.shape
+
+        # scannet: with_rot = False
+        dir_angle = distance.new_zeros(batch_size, num_proposal, 1) 
+
+        # decode bbox size
+        bbox_size = distance[..., 0:3] + distance[..., 3:6]
+        bbox_size = torch.clamp(bbox_size, min=0.1)
+
+        # decode bbox center
+        canonical_xyz = (distance[..., 3:6] -
+                         distance[..., 0:3]) / 2  # (batch_size, num_proposal, 3)
+
+        shape = canonical_xyz.shape
+
+        canonical_xyz = rotation_3d_in_axis(
+            canonical_xyz.view(-1, 3).unsqueeze(1),
+            dir_angle.view(-1),
+            axis=2
+        ).squeeze(1).view(shape)
+
+        ref_points = bbox_out['aggregated_vote_xyz']
+        center = ref_points - canonical_xyz
+
+        bbox3d = torch.cat([center, bbox_size, dir_angle], dim=-1)
+
+        return bbox3d
+
+
+class RefineProposalModule(nn.Module):
+    def __init__(self, num_class, num_heading_bin):
+        super().__init__()
+
+        self.num_class = num_class
+        self.num_heading_bin = num_heading_bin
+
+        # Object proposal/detection
+        # Distance (6), Angle (1) + num_class
+
+        self.proposal = nn.Sequential(
+            nn.Conv1d(256,128,1, bias=False),
+            nn.BatchNorm1d(128),
+            nn.ReLU(),
+            nn.Conv1d(128,128,1, bias=False),
+            nn.BatchNorm1d(128),
+            nn.ReLU(),
+            nn.Conv1d(128,6+1+self.num_class,1)
+        )
+
+    def forward(self, data_dict):
+        """
+        Args:
+            data_dict
+        Returns:
+            scores: (B,num_proposal,6 (distance) + 1 (heading) + num_class) 
+        """
+
+        features = data_dict['fused_feats'] # (batch_size, 256, num_proposal)
+        # --------- PROPOSAL GENERATION ---------
+        net = self.proposal(features)
+        data_dict = self.decode_scores(net, data_dict, self.num_class, self.num_heading_bin)
+
+        return data_dict
+
+    def decode_scores(self, net, data_dict, num_class, num_heading_bin):
+        """
+        decode the predicted parameters for the bounding boxes
+
+        """
+        net_transposed = net.transpose(2,1).contiguous() # (batch_size, 1024, ..)
+        batch_size = net_transposed.shape[0]
+        num_proposal = net_transposed.shape[1]
+
+        distance = data_dict['distance'] # (batch_size, num_proposal, 6)
+        angle = data_dict['proposal_list'][..., -1]
+
+        angle_delta = net_transposed[..., :1].squeeze(-1)
+        distance_delta = net_transposed[..., 1:7]
+        sem_cls_scores = net_transposed[:,:,7:] # Bxnum_proposalxnum_class
+
+        refined_distance = distance + distance_delta # (B, N, 6)
+
+        refined_angle = angle + angle_delta
+        refined_angle[refined_angle > np.pi] -= 2 * np.pi
+
+        data_dict['refined_distance'] = refined_distance  # (B, N, 6)
+        data_dict['refined_angle'] = refined_angle  # (B, N)
+        data_dict['refined_sem_cls_scores'] = sem_cls_scores
+
+        return data_dict
