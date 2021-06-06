a1cd5d54529cc128346d0a20692e8e86d1cdfc48
alperkesen <kesen.alper@gmail.com>
2021-06-06 16:56:41 +0300

    Fix batch size referenced before assignment in rpg module

--- models/rpg_module.py
+++ models/rpg_module.py
@@ -41,10 +41,11 @@ class RPGModule(nn.Module):
         seed_features = data_dict["seed_features"]
         ref_points = data_dict["aggregated_vote_xyz"]
         distance = data_dict["distance"]
-        data_dict["angle"] = distance.new_zeros(batch_size, num_proposal, 1)
-        
+
         batch_size, num_proposal, _ = distance.shape
+        data_dict["angle"] = distance.new_zeros(batch_size, num_proposal, 1)
         angle = data_dict["angle"][..., -1]
+
         if self.rep_type == 'grid':
             # (B, N, num_rep_points, 3)
             rep_points = self._get_grid_based_rep_points(
